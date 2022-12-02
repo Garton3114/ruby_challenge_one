748d7864c95791f7147e17e2ca0be1395f0496e4
Zac <64330632+Garton3114@users.noreply.github.com>
2022-12-02 16:07:06 +0000

    ready to be reviewed

--- chapter1/challenges/drills/lib/4_defining_methods.rb
+++ chapter1/challenges/drills/lib/4_defining_methods.rb
@@ -25,44 +25,82 @@
 # takes no args (arguments)
 # returns 'hello'
 
+def say_hello
+    return "hello" 
+end
+
 # say_goodbye
 # takes no args
 # returns 'goodbye'
+
+def say_goodbye
+    return "goodbye"
+end
 
 # say_hello_to
 # takes one string as an arg
 # example: the arg is `Sam`
 # returns `Hello, Sam`
 
+def say_hello_to(name)
+    return "Hello, " + (name)
+end
+
 # say_goodbye_to
 # takes one string as an arg
 # example: the arg is `Sam`
 # returns `Goodbye, Sam`
+
+def say_goodbye_to(name)
+    return "Goodbye, " + (name)
+end
 
 # square
 # takes one number as an arg
 # multiplies that number by itself
 # returns the new value
 
+def square(number)
+    return number * number
+end
+
 # divisible_by_three?
 # takes one number as an arg
 # returns true if the number is divisible by three
 # returns false if the number is not divisible by three
+
+def divisible_by_three?(number)
+    return number % 3 == 0 
+end
 
 # add
 # takes two numbers as args
 # adds them together
 # returns the total
 
+def add(number1, number2)
+    return number1 + number2
+end
+
 # multiply
 # takes two numbers as args
 # multiplies one by the other
 # returns the result
 
+def multiply(number1, number2)
+    return number1 * number2
+end
+
 # add_number_strings
 # takes two numbers as strings
 # converts them both to numbers
 # returns the total
+
+def add_number_strings(string1, string2)
+    number1 = string1.to_i
+    number2 = string2.to_i
+    return  number1 + number2
+end 
 
 # multiply_number_strings
 # takes two numbers as strings for args
@@ -70,25 +108,47 @@
 # multiplies one by the other
 # returns the result
 
+def multiply_number_strings(string1, string2)
+    number1 = string1.to_i
+    number2 = string2.to_i
+    return number1 * number2
+end
+
 # both_odd?
 # takes two numbers as args
 # returns true if both are odd
 # otherwise, returns false
+
+def both_odd?(num1, num2)
+    return num1.odd? && num2.odd?
+end
 
 # both_even?
 # takes two numbers as args
 # returns true if both are even
 # otherwise, returns false
 
+def both_even?(num1, num2)
+    return num1.even? && num2.even?
+end
+
 # one_odd?
 # takes two numbers as args
 # returns true if at least one of them is odd
 # otherwise, returns false
 
+def one_odd?(num1, num2)
+    return num1.odd? || num2.odd?
+end
+
 # one_even?
 # takes two numbers as args
 # returns true if at least one of them is even
 # otherwise returns false
+
+def one_even?(num1, num2)
+    return num1.even? || num2.even?
+end
 
 # rev_sym_caps
 # takes one string as an arg
@@ -96,9 +156,23 @@
 # converts it to a symbol
 # returns the symbol in block caps
 
+def rev_sym_caps(string)
+    caps = string.reverse.to_sym
+    return caps.upcase
+end
+
 # truncate
 # takes one string as an arg
 # if the string is longer than 10 characters
 # returns the first 10 chars followed by '...'
 # if the string is 10 chars or less
 # returns the whole string
+
+def truncate(string)
+    if string.length > 10 
+        n = 10
+        return string[0...n] +"..."
+    else 
+        return string
+    end
+end
