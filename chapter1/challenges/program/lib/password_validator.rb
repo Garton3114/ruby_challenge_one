8d2f20230dc21b380a3755366bd079931f7ce849
Zac <64330632+Garton3114@users.noreply.github.com>
2022-12-02 17:28:50 +0000

    PW validator

--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -3,4 +3,12 @@
 # - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
+    length = password.length >= 8
+    special = password =~ (/[!@$%&]/)
+
+    if length && special 
+        return true
+    else
+        return false
+    end
 end
